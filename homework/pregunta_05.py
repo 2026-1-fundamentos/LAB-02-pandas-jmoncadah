4a0f16cbab9e486f4168598cfc658fd6dc195b72
jmoncadah <jmoncadah@unal.edu.co>
2026-06-19 14:23:35 -0500

    dd

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -9,21 +9,21 @@ librerias de pandas para resolver las preguntas.
 import pathlib
 import pandas as pd
 
-def pregunta_04():
+def pregunta_05():
     """
-    Calcule el promedio de `c2` por cada letra de la `c1` del archivo
-    `tbl0.tsv`.
+    Calcule el valor máximo de `c2` por cada letra en la columna `c1` del
+    archivo `tbl0.tsv`.
 
     Rta/
     c1
-    A    4.625000
-    B    5.142857
-    C    5.400000
-    D    3.833333
-    E    4.785714
-    Name: c2, dtype: float64
+    A    9
+    B    9
+    C    9
+    D    7
+    E    9
+    Name: c2, dtype: int64
     """
     current_dir = pathlib.Path(__file__).parent
     path = current_dir.parent / "files" / "input" / "tbl0.tsv"
     tbl0 = pd.read_csv(path, sep="\t")
-    return tbl0.groupby("c1")["c2"].mean().sort_index()
+    return tbl0.groupby("c1")["c2"].max().sort_index()
